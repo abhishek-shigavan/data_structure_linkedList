f479bf392f5f7e551cc05a60f63db427af6f797e
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-24 23:00:17 +0530

    Adding New Node at Front

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -44,4 +44,35 @@ public class MyLinkedList <E> {
             this.tail = newNode;
         }
     }
+/**
+ * In this method --
+ * If new node is first node of LinkedList
+ * then it will make it head & tail both
+ * Else new node will be added to front of
+ * head
+ *
+ * @param newNode
+ *
+ * @return No return
+ */
+    public void addNodeAtFront(INode<E> newNode) {
+
+        if(this.tail == null) {
+            //making new node as tail
+            this.tail = newNode;
+        }
+
+        if(this.head == null) {
+            //making new node as head
+            this.head = newNode;
+        }
+        else {
+            //storing current head node into temp node
+            INode<E> tempNode = this.head;
+            //making new node as a head
+            this.head = newNode;
+            //setting temp node next to head node
+            this.head.setNext(tempNode);
+        }
+    }
 }
