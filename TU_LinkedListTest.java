5752ef36b23587073efc031547acf3063b19a306
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-24 23:13:01 +0530

    Add: Test Cases

--- TU_LinkedListTest.java
+++ TU_LinkedListTest.java
@@ -38,4 +38,20 @@ class TU_LinkedListTest {
                 myLinkedList.tail.equals(myFirstNode);
         Assertions.assertTrue(result);
     }
+    //for append
+    @Test
+    void when3NumbersAddedToLinkedListLastNoShouldBeAtTail() {
+
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addNodeAtTail(myFirstNode);
+        myLinkedList.addNodeAtTail(mySecondNode);
+        myLinkedList.addNodeAtTail(myThirdNode);
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myThirdNode);
+        Assertions.assertTrue(result);
+    }
 }
